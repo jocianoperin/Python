f20dfe49d44f3cac409614e3e4b935ee102f7098
Jociano Perin <jociano.perin@outlook.com>
2020-11-02 23:10:17 -0300

    teste2

--- teste.py
+++ teste.py
@@ -1,13 +1,8 @@
 lista_quadrados = [0,1,4,9,16,25]
 print(lista_quadrados)
 print(lista_quadrados[2:4])
-'''Teste
-de 
-comentário
-com
-quebra
-de
-linha'''
+'''Teste de comentário
+com quebra de linha'''
 
 exemplo_tupla = 1, 2, 3
 print(exemplo_tupla)
